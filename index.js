21412021b77f813842978f6884b1b6d9f846d424
zhangyunlong <zhangyunlong@baidu.com>
2013-08-09 23:40:32 +0800

    发布v0.0.4
    * 升级jshint至v2.1.9
    * 修复不配置ignored就报错的bug

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ var JSHINT = require("jshint").JSHINT;
 var ignored, globals;
 
 module.exports = function(content, file, conf){
-	if(conf.ignored){
+    if(conf.ignored){
         if(typeof conf.ignored === 'string' || fis.util.is(conf.ignored, 'RegExp')){
             ignored = [ conf.ignored ];
         } else if(fis.util.is(conf.ignored, 'Array')) {
             ignored = conf.ignored;
         }
-		delete conf.ignored;
-	}
+        delete conf.ignored;
+    }
+    if(ignored){
+        for(var i = 0, len = ignored.length; i < len; i++){
+            if(fis.util.filter(file.subpath, ignored[i])) return;
+        }
+    }
     if(conf.globals){
         globals = conf.globals;
         delete conf.globals;
-    }
-    for(var i = 0, len = ignored.length; i < len; i++){
-        if(fis.util.filter(file.subpath, ignored[i])) return;
     }
     delete conf.filename;
     if(!JSHINT(content, conf, globals)){
